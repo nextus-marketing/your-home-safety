2491d42dc40639f2f99da60a6af879ba51a2ba99
Sidh2003 <siddheshsonavane22@gmail.com>
2026-04-26 16:38:34 +0530

    d

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -44,9 +44,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
 </head>
-<a href="tel:+18664206110" class="call-float">
+{{-- <a href="tel:+18664206110" class="call-float">
     <i class="fa fa-phone"></i>
-</a>
+</a> --}}
 <style>
     .topbar-info-text p {
         color: var(--white-color);
